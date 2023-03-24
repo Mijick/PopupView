83737c053e12a22340848ca8e239d007fd82484f
Tomasz Kurylik <fulcrumone@icloud.com>
2023-03-24 01:22:31 +0100

    1

--- Sources/PopupView/Views/PopupBottomStackView.swift
+++ Sources/PopupView/Views/PopupBottomStackView.swift
@@ -94,7 +94,7 @@ private extension PopupBottomStackView {
     func saveHeight(_ height: CGFloat, for item: AnyPopup<BottomPopupConfig>) {
         switch config.contentFillsWholeHeight {
             case true: heights[item] = getMaxHeight()
-            case false: heights[item] = min(height, getMaxHeight() - (config.contentIgnoresSafeArea ? 0 : UIScreen.safeArea.bottom))
+            case false: heights[item] = min(height, getMaxHeight() - bottomPadding)
         }
     }
     func getMaxHeight() -> CGFloat {
