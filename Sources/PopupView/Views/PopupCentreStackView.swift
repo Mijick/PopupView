84d2f3f9c67ff9f2360f78ba9c2f62875e0c9f0c
Tomasz Kurylik <fulcrumone@icloud.com>
2023-03-29 12:45:47 +0200

    1

--- Sources/PopupView/Views/PopupCentreStackView.swift
+++ Sources/PopupView/Views/PopupCentreStackView.swift
@@ -24,14 +24,14 @@ struct PopupCentreStackView: View {
             .frame(width: UIScreen.width, height: UIScreen.height)
             .background(createTapArea())
             .animation(transitionAnimation, value: height)
-            //.animation(transitionAnimation, value: items.isEmpty)
+            .animation(transitionAnimation, value: items.isEmpty)
             .transition(
-                .scale(scale: items.isEmpty ? 0.9 : 1.1).combined(with: .opacity).animation(height == nil || items.isEmpty ? transitionAnimation : nil)
+                //.scale(scale: items.isEmpty ? 0.9 : 1.1).combined(with: .opacity).animation(height == nil || items.isEmpty ? transitionAnimation : nil)
 
 
-//                .asymmetric(
-//                insertion: .scale(scale: 1.1).combined(with: .opacity).animation(height == nil ? transitionAnimation : nil),
-//                removal: .scale(scale: 1.1).combined(with: .opacity).animation(items.count == 0 ? transitionAnimation : nil))
+                .asymmetric(
+                insertion: .scale(scale: 1.1).combined(with: .opacity).animation(height == nil ? transitionAnimation : nil),
+                removal: .scale(scale: 1.1).combined(with: .opacity).animation(items.count == 0 ? transitionAnimation : nil))
             )
             .onChange(of: items, perform: onItemsChange)
     }
