ac307db1ecb8bb7d7702fe1e2fb35d65725180d0
Tomasz Kurylik <fulcrumone@icloud.com>
2023-03-29 03:10:17 +0200

    1

--- Sources/PopupView/Views/PopupCentreStackView.swift
+++ Sources/PopupView/Views/PopupCentreStackView.swift
@@ -20,9 +20,9 @@ struct PopupCentreStackView: View {
             if items.isEmpty { EmptyView() }
             else { createPopup() }
         }
-        .animation(transitionAnimation, value: items)
+        //.animation(transitionAnimation, value: items)
         .transition(
-            .asymmetric(insertion: .scale(scale: 1.1).combined(with: .opacity), removal: .scale(scale: 0.9).combined(with: .opacity))
+            .asymmetric(insertion: .scale(scale: 1.1).combined(with: .opacity).animation(transitionAnimation), removal: .scale(scale: 0.9).combined(with: .opacity).animation(transitionAnimation))
         )
 
 
