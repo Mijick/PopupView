5f7d647eb1f0c8cee4674046d2a7cb697d1c6cb5
Tomasz Kurylik <fulcrumone@icloud.com>
2023-03-29 12:04:52 +0200

    1

--- Sources/PopupView/Views/PopupCentreStackView.swift
+++ Sources/PopupView/Views/PopupCentreStackView.swift
@@ -12,7 +12,7 @@ import SwiftUI
 
 struct PopupCentreStackView: View {
     let items: [AnyPopup<CentrePopupConfig>]
-    @State private var height: CGFloat?
+    @State private var heights: [AnyPopup<CentrePopupConfig>: CGFloat] = [:]
 
 
     var body: some View {
@@ -21,14 +21,14 @@ struct PopupCentreStackView: View {
             .background(createTapArea())
             .animation(transitionAnimation, value: height)
             .transition(.asymmetric(insertion: .opacity.animation(transitionAnimation), removal: .scale(scale: 0.9).combined(with: .opacity).animation(transitionAnimation)))
-            .onChange(of: items, perform: onItemsChange)
+            //.onChange(of: items, perform: onItemsChange)
     }
 }
 
 private extension PopupCentreStackView {
     func createPopup() -> some View {
         items.last?.body
-            .readHeight(onChange: getHeight)
+            .readHeight { saveHeight($0, for: items.last!) }
             .frame(width: width, height: height)
             .background(backgroundColour)
             .cornerRadius(cornerRadius)
@@ -45,18 +45,18 @@ private extension PopupCentreStackView {
 // MARK: -Logic Handlers
 private extension PopupCentreStackView {
     func onItemsChange(_ items: [AnyPopup<CentrePopupConfig>]) {
-        if items.isEmpty { height = nil }
+        //if items.isEmpty { height = nil }
     }
 }
 
 // MARK: -View Handlers
 private extension PopupCentreStackView {
-    func getHeight(_ value: CGFloat) { height = value }
+    func saveHeight(_ height: CGFloat, for item: AnyPopup<CentrePopupConfig>) { heights[item] = height }
 }
 
 private extension PopupCentreStackView {
     var width: CGFloat { max(0, UIScreen.width - config.horizontalPadding * 2) }
-    var opacity: Double { (height != nil).doubleValue }
+    var height: CGFloat? { heights.first { $0.key == items.last }?.value }
     var cornerRadius: CGFloat { config.cornerRadius }
     var scale: CGFloat { height == nil ? 1.1 : 1 }
     var backgroundColour: Color { config.backgroundColour }
