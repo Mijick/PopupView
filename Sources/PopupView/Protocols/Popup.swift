4d2918717ad08d8f40b384ae37d981ed961a655e
Tomasz Kurylik <fulcrumone@icloud.com>
2023-05-21 22:55:19 +0200

    1

--- Sources/PopupView/Protocols/Popup.swift
+++ Sources/PopupView/Protocols/Popup.swift
@@ -20,7 +20,7 @@ public protocol Popup: View {
     func configurePopup(popup: Config) -> Config
 }
 
-// MARK: - Presenting Popups
+// MARK: - Presenting and Dismissing
 public extension Popup {
 
     /// Displays the popup. Stacks previous one
@@ -29,8 +29,6 @@ public extension Popup {
     /// Displays the popup. Closes previous one
     func showAndReplace() { PopupManager.show(AnyPopup<Config>(self), withStacking: false) }
 }
-
-// MARK: - Dismissing Popups
 public extension Popup {
 
     /// Dismisses the last popup on the stack
@@ -45,40 +43,15 @@ public extension Popup {
 
 // MARK: - Others
 public extension Popup {
-    func configurePopup(popup: Config) -> Config { popup }
-
     var id: String { .init(describing: Self.self) }
     var body: V { createContent() }
+
+    func configurePopup(popup: Config) -> Config { popup }
 }
 
-// MARK: - Deprecated
+
+// MARK: - Deprecated Methods
 public extension Popup {
     @available(*, deprecated, message: "Method no longer supported. Use showAndStack or showAndReplace instead")
     func present() { PopupManager.show(AnyPopup<Config>(self), withStacking: true) }
-}
-
-
-public protocol TopPopup: Popup { associatedtype Config = TopPopupConfig }
-public protocol CentrePopup: Popup { associatedtype Config = CentrePopupConfig }
-public protocol BottomPopup: Popup { associatedtype Config = BottomPopupConfig }
-
-struct AnyPopup<Config: Configurable>: Popup, Hashable {
-    let id: String
-
-    private let _body: AnyView
-    private let _configBuilder: (Config) -> Config
-
-    init(_ popup: some Popup) {
-        self.id = popup.id
-        self._body = AnyView(popup)
-        self._configBuilder = popup.configurePopup as! (Config) -> Config
-    }
-}
-extension AnyPopup {
-    static func == (lhs: AnyPopup<Config>, rhs: AnyPopup<Config>) -> Bool { lhs.id == rhs.id }
-    func hash(into hasher: inout Hasher) { hasher.combine(id) }
-}
-extension AnyPopup {
-    func createContent() -> some View { _body }
-    func configurePopup(popup: Config) -> Config { _configBuilder(popup) }
 }
