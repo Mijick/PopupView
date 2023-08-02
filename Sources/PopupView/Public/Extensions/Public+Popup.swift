d3bd787586069adf56dcff5a6860486d2bae1536
Tomasz Kurylik <fulcrumone@icloud.com>
2023-08-02 14:48:54 +0200

    Moved dismiss popup methods to VIEW extension

--- Sources/PopupView/Public/Extensions/Public+Popup.swift
+++ Sources/PopupView/Public/Extensions/Public+Popup.swift
@@ -10,26 +10,13 @@
 
 import SwiftUI
 
-// MARK: - Presenting and Dismissing
+// MARK: - Presenting
 public extension Popup {
     /// Displays the popup. Stacks previous one
     func showAndStack() { PopupManager.showAndStack(AnyPopup<Config>(self)) }
 
     /// Displays the popup. Closes previous one
     func showAndReplace() { PopupManager.showAndReplace(AnyPopup<Config>(self)) }
-}
-public extension Popup {
-    /// Dismisses the last popup on the stack
-    func dismiss() { PopupManager.dismiss() }
-
-    /// Dismisses all popups of the selected type on the stack
-    func dismiss<P: Popup>(_ popup: P.Type) { PopupManager.dismiss(popup) }
-
-    /// Dismisses all popups on the stack up to the popup with the selected type
-    func dismissAll<P: Popup>(upTo popup: P.Type) { PopupManager.dismissAll(upTo: popup) }
-
-    /// Dismisses all popups on the stack
-    func dismissAll() { PopupManager.dismissAll() }
 }
 
 // MARK: - Available Popups
