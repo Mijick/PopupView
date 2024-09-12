ba52c52ec379edefe35f64a4b313b047679aaec2
Tomasz Kurylik <fulcrumone@icloud.com>
2024-09-12 15:27:36 +0200

    1

--- Sources/Public/Extensions/Public+PopupManager.swift
+++ Sources/Public/Extensions/Public+PopupManager.swift
@@ -13,21 +13,24 @@ import SwiftUI
 // MARK: - Presenting and Dismissing
 public extension PopupManager {
     /// Displays the popup. Stacks previous one
-    static func showAndStack(_ popup: some Popup) { performOperation(.insertAndStack(popup)) }
+    static func showAndStack(_ popup: some Popup) { performOperation(.insertAndStack(.init(popup))) }
 
     /// Displays the popup. Closes previous one
-    static func showAndReplace(_ popup: some Popup) { performOperation(.insertAndReplace(popup)) }
+    static func showAndReplace(_ popup: some Popup) { performOperation(.insertAndReplace(.init(popup))) }
 }
 public extension PopupManager {
-    /// Dismisses last popup on the stack
+    /// Dismisses the last popup on the stack
     static func dismiss() { performOperation(.removeLast) }
 
-    /// Dismisses all popups of provided type on the stack.
-    static func dismiss<P: Popup>(_ popup: P.Type) { performOperation(.remove(ID(popup))) }
+    /// Dismisses all the popups of provided ID on the stack
+    static func dismissPopup(id: String) { performOperation(.remove(.init(value: id))) }
 
-    /// Dismisses all popups on the stack up to the popup with the selected type
+    /// Dismisses all the popups of provided type on the stack
+    static func dismissPopup<P: Popup>(_ popup: P.Type) { performOperation(.remove(ID(popup))) }
+
+    /// Dismisses all the popups on the stack up to the popup with the selected type
     static func dismissAll<P: Popup>(upTo popup: P.Type) { performOperation(.removeAllUpTo(ID(popup))) }
 
-    /// Dismisses all the popups on the stack.
+    /// Dismisses all the popups on the stack
     static func dismissAll() { performOperation(.removeAll) }
 }
