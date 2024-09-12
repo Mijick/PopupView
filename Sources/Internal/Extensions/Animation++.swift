544458f4354a02ae0e8f8569882b2c7df9df3ae2
Tomasz Kurylik <fulcrumone@icloud.com>
2024-09-13 00:29:09 +0200

    ANIMATIONS FIX

--- Sources/Internal/Extensions/Animation++.swift
+++ Sources/Internal/Extensions/Animation++.swift
@@ -13,4 +13,6 @@ import SwiftUI
 
 extension Animation {
     static var keyboard: Animation { .interpolatingSpring(mass: 3, stiffness: 1000, damping: 500, initialVelocity: 6.4) }
+    static var transition: Animation { .spring(duration: 0.48, bounce: 0, blendDuration: 0) }
+    static var drag: Animation { .spring(duration: 0.12, bounce: 0, blendDuration: 0) }
 }
