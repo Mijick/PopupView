50e52312790c53d6683e4de9f453ab99e7f82f58
Tomasz Kurylik <fulcrumone@icloud.com>
2024-09-14 23:06:49 +0200

    1

--- Sources/Public/Configurables/Public+GlobalConfig.swift
+++ Sources/Public/Configurables/Public+GlobalConfig.swift
@@ -20,7 +20,11 @@ public extension GlobalConfig.Vertical {
 
 // MARK: - Centre
 public extension GlobalConfig.Centre {
-    
+    /// Scale of the initial state of the popup animation while opening
+    func transitionEntryScale(_ value: CGFloat) -> Self { self.transitionEntryScale = value; return self }
+
+    /// Scale of the final state of the popup animation while closing
+    func transitionExitScale(_ value: CGFloat) -> Self { self.transitionExitScale = value; return self }
 }
 
 
@@ -30,17 +34,17 @@ public extension GlobalConfig.Centre {
 
 public extension GlobalConfig {
     /// Background colour of the popup
-    func backgroundColour(_ value: Color) -> Self { changing(path: \.backgroundColour, to: value) }
+    func backgroundColour(_ value: Color) -> Self { self.backgroundColour = value; return self }
 
     /// Corner radius of the popup at the top of the stack
-    func cornerRadius(_ value: CGFloat) -> Self { changing(path: \.cornerRadius, to: value) }
+    func cornerRadius(_ value: CGFloat) -> Self { self.cornerRadius = value; return self }
 
     /// Applies shadows to the popup
-    func applyShadow(color: Color = .black.opacity(0.16), radius: CGFloat = 16, x: CGFloat = 0, y: CGFloat = 0) -> Self { changing(path: \.shadow, to: .init(color: color, radius: radius, x: x, y: y)) }
+    func applyShadow(color: Color = .black.opacity(0.16), radius: CGFloat = 16, x: CGFloat = 0, y: CGFloat = 0) -> Self { self.shadow = .init(color: color, radius: radius, x: x, y: y); return self }
 
     /// Dismisses the active popup when tapped outside its area if enabled
-    func tapOutsideToDismiss(_ value: Bool) -> Self { changing(path: \.tapOutsideClosesView, to: value) }
+    func tapOutsideToDismiss(_ value: Bool) -> Self { self.tapOutsideClosesView = value; return self }
 
     /// Colour of the overlay covering the view behind the popup. Use .clear to hide the overlay
-    func overlayColour(_ value: Color) -> Self { changing(path: \.overlayColour, to: value) }
+    func overlayColour(_ value: Color) -> Self { self.overlayColour = value; return self }
 }
