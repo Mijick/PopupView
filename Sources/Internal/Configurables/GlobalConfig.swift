5e557862cf5343fa4b06bd3d3a173715daa6b1af
Tomasz Kurylik <fulcrumone@icloud.com>
2024-09-14 22:19:57 +0200

    1

---
+++ Sources/Internal/Configurables/GlobalConfig.swift
@@ -0,0 +1,40 @@
+//
+//  GlobalConfig.swift of MijickPopupView
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//
+//  Copyright ©2024 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+public class GlobalConfig111: Configurable { public required init() {}
+    var backgroundColour: Color = .white
+    var cornerRadius: CGFloat = 28
+    var shadow: Shadow = .none
+    var tapOutsideClosesView: Bool = false
+    var overlayColour: Color = .black.opacity(0.44)
+}
+
+// MARK: Vertical
+public extension GlobalConfig111 { class Vertical: GlobalConfig111 {
+    var distanceFromKeyboard: CGFloat = 8
+    var stackCornerRadiusMultiplier: CGFloat = 0.6
+    var stackOffset: CGFloat = 8
+    var stackScaleFactor: CGFloat = 0.05
+    var stackLimit: Int = 4
+    var dragGestureEnabled: Bool = true
+    var dragGestureProgressToClose: CGFloat = 1/3
+}}
+
+// MARK: Centre
+public extension GlobalConfig111 { class Centre: GlobalConfig111 {
+    var transitionEntryScale: CGFloat = 1.16
+    var transitionExitScale: CGFloat = 0.82
+}}
+
+// MARK: Shared
+public extension GlobalConfig111 { class Shared: GlobalConfig111 {}}
