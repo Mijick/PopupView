02ebf0145bdec9811ff447ef2f56e6c675755d19
Tomasz Kurylik <fulcrumone@icloud.com>
2024-09-17 13:02:14 +0200

    1

--- Sources/Internal/View Modifiers/HeightReader.swift
+++ Sources/Internal/View Modifiers/HeightReader.swift
@@ -18,14 +18,12 @@ extension View {
 fileprivate struct Modifier: ViewModifier {
     let onHeightChange: (CGFloat) -> ()
 
-    func body(content: Content) -> some View { content.background(
-        GeometryReader { geometry in Color.clear
-            .preference(key: SizePreferenceKey.self, value: geometry.size)
-            .onPreferenceChange(SizePreferenceKey.self) { onHeightChange($0.height) }
-        }
-    )}
-    struct SizePreferenceKey: PreferenceKey {
-        static var defaultValue: CGSize = .zero
-        static func reduce(value: inout CGSize, nextValue: () -> CGSize) { value = nextValue() }
+    func body(content: Content) -> some View { content
+        .background(
+            GeometryReader { geo -> Color in
+                onHeightChange(geo.size.height)
+                return Color.clear
+            }
+        )
     }
 }
