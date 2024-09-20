1d1368532387ad8a3a291bbbb72624dc802def49
Tomasz Kurylik <fulcrumone@icloud.com>
2024-09-20 12:41:34 +0200

    1

--- Sources/Internal/Extensions/Animation++.swift
+++ Sources/Internal/Extensions/Animation++.swift
@@ -12,6 +12,5 @@
 import SwiftUI
 
 extension Animation {
-    static var keyboard: Animation { .interpolatingSpring(mass: 3, stiffness: 1000, damping: 500, initialVelocity: 6.4) }
     static var transition: Animation { .spring(duration: 0.4, bounce: 0, blendDuration: 0) }
 }
