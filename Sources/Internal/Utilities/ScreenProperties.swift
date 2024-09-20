5a9f6ee9f72de0b8a057c67973c8030f617ad104
Tomasz Kurylik <fulcrumone@icloud.com>
2024-09-20 15:37:37 +0200

    1

---
+++ Sources/Internal/Utilities/ScreenProperties.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenProperties.swift of MijickPopups
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//
+//  Copyright ©2024 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+struct ScreenProperties: Equatable {
+    var height: CGFloat = .zero
+    var safeArea: EdgeInsets = .init()
+}
