2a4277f3dd16525db3ffb9c4deec44551c018184
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-04 18:16:30 +0200

    1

---
+++ Tests/Tests+PopupManager.swift
@@ -0,0 +1,27 @@
+//
+//  Tests+PopupManager.swift of MijickPopups
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//
+//  Copyright ©2024 Mijick. Licensed under MIT License.
+
+
+import XCTest
+import SwiftUI
+import Combine
+@testable import MijickPopups
+
+final class PopupManagerTests: XCTestCase {
+
+
+
+
+    // co testować:
+    // 1. Tworzenie instancji
+    // 2. Pobieranie instancji o danym id
+    // 3. Operacje na views
+    
+}
