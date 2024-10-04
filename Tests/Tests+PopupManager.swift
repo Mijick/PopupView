c21f3146aff35be9d29432157eeaedebc624e996
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-04 18:58:47 +0200

    1

--- Tests/Tests+PopupManager.swift
+++ Tests/Tests+PopupManager.swift
@@ -25,3 +25,66 @@ final class PopupManagerTests: XCTestCase {
     // 3. Operacje na views
     
 }
+
+
+
+// MARK: - TEST CASES
+
+
+
+// MARK: Register New Instance
+extension PopupManagerTests {
+    func test_registerNewInstance_1() {
+        PopupManager.registerNewInstance(id: .shared)
+
+        PopupManagerRegistry.instances.count
+    }
+    func test_registerNewInstance_2() {
+        PopupManager.registerNewInstance(id: .shared)
+
+        PopupManagerRegistry.instances.count
+    }
+    func test_registerNewInstance_3() {
+        PopupManager.registerNewInstance(id: .shared)
+
+        PopupManagerRegistry.instances.count
+    }
+}
+
+// MARK: Get Instance
+extension PopupManagerTests {
+
+}
+
+// MARK: Insert Operations
+extension PopupManagerTests {
+    
+}
+
+// MARK: Remove Operations
+extension PopupManagerTests {
+
+}
+
+
+// MARK: - HELPERS
+
+
+
+// MARK: Methods
+private extension PopupManagerTests {
+
+}
+private extension PopupManagerTests {
+
+}
+
+// MARK: Popup Manager Identifiers
+private extension PopupManagerID {
+    static let staremiasto: Self = .init(rawValue: "staremiasto")
+    static let grzegorzki: Self = .init(rawValue: "grzegorzki")
+    static let pradnikczerwony: Self = .init(rawValue: "pradnikczerwony")
+    static let pradnikbialy: Self = .init(rawValue: "pradnikbialy")
+    static let krowodrza: Self = .init(rawValue: "krowodrza")
+    static let bronowice: Self = .init(rawValue: "bronowice")
+}
