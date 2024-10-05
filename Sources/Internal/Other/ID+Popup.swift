2725bd250bb61fb0ede25f5c9a5f5aadee38d14d
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-05 21:35:21 +0200

    1

--- Sources/Internal/Other/ID+Popup.swift
+++ Sources/Internal/Other/ID+Popup.swift
@@ -13,28 +13,36 @@ import Foundation
 
 public struct PopupID {
     let rawValue: String
+}
 
-    // MARK: Initialiser
-    init(rawValue: String) { self.rawValue = rawValue + Self.separator + .init(describing: Date()) }
-    init<P: Popup>(_ type: P.Type) { self.rawValue = .init(describing: P.self) + Self.separator + .init(describing: Date()) }
-    init<P: Popup>(_ object: P) { self.init(P.self) }
+extension PopupID {
+    static func create(from id: String) -> Self {
+        let firstComponent = id,
+            secondComponent = separator,
+            thirdComponent = String(describing: Date())
+        return .init(rawValue: firstComponent + secondComponent + thirdComponent)
+    }
+    static func create(from popupType: any Popup.Type) -> Self {
+        create(from: .init(describing: popupType))
+    }
 }
 
 // MARK: - Equatable
-extension PopupID: Equatable {
-    public static func ==(lhs: Self, rhs: Self) -> Bool { lhs.rawValue == rhs.rawValue }
-    public static func ~=(lhs: Self, rhs: Self) -> Bool { getComponent(lhs) == getComponent(rhs) }
+extension PopupID {
+    func isSameType(as id: String) -> Bool { getFirstComponent(of: self) == id }
+    func isSameType(as popupType: any Popup.Type) -> Bool { getFirstComponent(of: self) == getFirstComponent(of: .create(from: popupType)) }
+    func isSameInstance(as id: PopupID) -> Bool { rawValue == id.rawValue }
 }
 
 // MARK: - Hashing
 extension PopupID: Hashable {
-    public func hash(into hasher: inout Hasher) { hasher.combine(Self.getComponent(self)) }
+    public func hash(into hasher: inout Hasher) { hasher.combine(getFirstComponent(of: self)) }
 }
 
 
 // MARK: - Helpers
 private extension PopupID {
-    static func getComponent(_ object: Self) -> String { object.rawValue.components(separatedBy: separator).first ?? "" }
+    func getFirstComponent(of object: Self) -> String { object.rawValue.components(separatedBy: Self.separator).first ?? "" }
 }
 private extension PopupID {
     static var separator: String { "/{}/" }
