c29774ab257a6917f26e4d466c701676fbdbf829
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-06 17:37:46 +0200

    1

--- Sources/Internal/Other/PopupManagerRegistry.swift
+++ Sources/Internal/Other/PopupManagerRegistry.swift
@@ -15,16 +15,17 @@ import Foundation
     static private(set) var instances: [PopupManager] = []
 }
 
+// MARK: Register
 extension PopupManagerRegistry {
-    static func registerNewInstance(_ popupManager: PopupManager) -> PopupManager {
-        if let alreadyRegisteredPopupManager = instances.first(where: { $0.id == popupManager.id }) { return alreadyRegisteredPopupManager }
+    static func register(popupManager: PopupManager) -> PopupManager {
+        if let alreadyRegisteredInstance = instances.first(where: { $0.id == popupManager.id }) { return alreadyRegisteredInstance }
 
         instances.append(popupManager)
         return popupManager
     }
 }
 
-
+// MARK: Clean
 extension PopupManagerRegistry {
     static func clean() { instances = [] }
 }
