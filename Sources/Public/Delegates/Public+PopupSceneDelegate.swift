715f51256fbce59c71c6daeb0705743d4803d383
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-07 14:08:05 +0200

    1

--- Sources/Public/Delegates/Public+PopupSceneDelegate.swift
+++ Sources/Public/Delegates/Public+PopupSceneDelegate.swift
@@ -34,11 +34,59 @@ extension PopupSceneDelegate {
 }
 
 
-// MARK: - Helpers
+
+// MARK: - WINDOW
+
+
+
+// MARK: iOS 18 and above
 fileprivate class Window: UIWindow {
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        if #available(iOS 18, *) {
+            guard let view = rootViewController?.view else { return false }
+
+            let hit = _hitTest(point, with: event, view: subviews.count > 1 ? self : view )
+            return hit != nil
+        } else {
+            return super.point(inside: point, with: event)
+        }
+    }
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        guard let view = super.hitTest(point, with: event) else { return nil }
-        return rootViewController?.view == view ? nil : view
+        if #available(iOS 18, *) {
+            return super.hitTest(point, with: event)
+        } else {
+            guard let hit = super.hitTest(point, with: event) else { return .none }
+            return rootViewController?.view == hit ? .none : hit
+        }
+    }
+}
+private extension Window {
+    func _hitTest(_ point: CGPoint, with event: UIEvent?, view: UIView, depth: Int = 0) -> Optional<(view: UIView, depth: Int)> {
+        var deepest: Optional<(view: UIView, depth: Int)> = .none
+
+        for subview in view.subviews.reversed() {
+            let converted = view.convert(point, to: subview)
+
+            guard subview.isUserInteractionEnabled,
+                  !subview.isHidden,
+                  subview.alpha > 0,
+                  subview.point(inside: converted, with: event)
+            else { continue }
+
+            let result = if let hit = _hitTest(converted, with: event, view: subview, depth: depth + 1) {
+                hit
+            } else  {
+                (view: subview, depth: depth)
+            }
+
+            if case .none = deepest {
+                deepest = result
+            } else if let current = deepest, result.depth > current.depth {
+                deepest = result
+            }
+        }
+
+        return deepest
     }
 }
 #endif
