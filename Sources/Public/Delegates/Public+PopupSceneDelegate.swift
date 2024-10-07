1bb9339a9928107fa6cd85b15ec2066e5a048a75
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-07 15:05:54 +0200

    1

--- Sources/Public/Delegates/Public+PopupSceneDelegate.swift
+++ Sources/Public/Delegates/Public+PopupSceneDelegate.swift
@@ -38,12 +38,13 @@ extension PopupSceneDelegate {
 
 
 
+// MARK: Implementation
 fileprivate class Window: UIWindow {
     override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
         if #available(iOS 18, *) {
             guard let view = rootViewController?.view else { return false }
 
-            let hit = iOS18_hitTest(point, with: event, view: subviews.count > 1 ? self : view)
+            let hit = hitTestHelper(point, with: event, view: subviews.count > 1 ? self : view)
             return hit != nil
         } else {
             return super.point(inside: point, with: event)
@@ -53,17 +54,17 @@ fileprivate class Window: UIWindow {
         if #available(iOS 18, *) {
             return super.hitTest(point, with: event)
         } else {
-            guard let hit = super.hitTest(point, with: event) else { return .none }
-            return rootViewController?.view == hit ? .none : hit
+            guard let hit = super.hitTest(point, with: event) else { return nil }
+            return rootViewController?.view == hit ? nil : hit
         }
     }
 }
 
-// MARK: Hit Test For iOS 18
+// MARK: Hit Test Helper
 // Based on philip_trauner solution: https://forums.developer.apple.com/forums/thread/762292?answerId=803885022#803885022
 @available(iOS 18, *)
 private extension Window {
-    func iOS18_hitTest(_ point: CGPoint, with event: UIEvent?, view: UIView, depth: Int = 0) -> HitTestResult? {
+    func hitTestHelper(_ point: CGPoint, with event: UIEvent?, view: UIView, depth: Int = 0) -> HitTestResult? {
         view.subviews.reversed().reduce(nil) { deepest, subview in let convertedPoint = view.convert(point, to: subview)
             guard shouldCheckSubview(subview, convertedPoint: convertedPoint, event: event) else { return deepest }
 
@@ -81,7 +82,7 @@ private extension Window {
         subview.point(inside: convertedPoint, with: event)
     }
     func calculateHitTestSubviewResult(_ point: CGPoint, with event: UIEvent?, subview: UIView, depth: Int) -> HitTestResult {
-        switch iOS18_hitTest(point, with: event, view: subview, depth: depth + 1) {
+        switch hitTestHelper(point, with: event, view: subview, depth: depth + 1) {
             case .some(let result): result
             case nil: (subview, depth)
         }
