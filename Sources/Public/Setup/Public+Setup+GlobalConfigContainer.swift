e8ea6076235641a5e592b6e6156f8e52e66aab1c
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-14 16:04:24 +0200

    1

--- Sources/Public/Setup/Public+Setup+GlobalConfigContainer.swift
+++ Sources/Public/Setup/Public+Setup+GlobalConfigContainer.swift
@@ -1,5 +1,5 @@
 //
-//  Public+GlobalConfigContainer.swift of MijickPopups
+//  Public+Setup+GlobalConfigContainer.swift of MijickPopups
 //
 //  Created by Tomasz Kurylik. Sending ❤️ from Kraków!
 //    - Mail: tomasz.kurylik@mijick.com
