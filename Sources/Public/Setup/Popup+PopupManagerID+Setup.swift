d92096b342afb1b9e5db6e4e17c0d8d62dbbd350
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-15 01:01:59 +0200

    1

--- Sources/Public/Setup/Popup+PopupManagerID+Setup.swift
+++ Sources/Public/Setup/Popup+PopupManagerID+Setup.swift
@@ -9,6 +9,37 @@
 //  Copyright ©2024 Mijick. All rights reserved.
 
 
+/**
+ Extending this structure allows popups to be registered on multiple screens.
+
+ # Usage Example
+ ```swift
+ @main struct App_Main: App {
+    var body: some Scene {
+        Window("Window1", id: "Window1") {
+            ContentView().registerPopups(id: .custom1)
+        }
+        Window("Window2", id: "Window2") {
+            ContentView().registerPopups(id: .custom2)
+        }
+    }
+ }
+
+ extension PopupManagerID {
+    static let custom1: Self = .init(rawValue: "custom1")
+    static let custom2: Self = .init(rawValue: "custom2")
+ }
+ ```
+
+ - important: Text
+ */
+public struct PopupManagerID: Equatable, Sendable {
+    let rawValue: String
+
+    public init(rawValue: String) { self.rawValue = rawValue }
+}
+
+// MARK: Default Instance
 public extension PopupManagerID {
     static let shared: Self = .init(rawValue: "shared")
 }
