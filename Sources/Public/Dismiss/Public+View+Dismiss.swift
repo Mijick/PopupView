b3f07b55267b56e36af340bff276ab940d6d2945
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-16 01:38:50 +0200

    1

--- Sources/Public/Dismiss/Public+View+Dismiss.swift
+++ Sources/Public/Dismiss/Public+View+Dismiss.swift
@@ -13,12 +13,25 @@ import SwiftUI
 
 public extension View {
     /**
-     Dismisses the currently active popup.
+     Removes the currently active popup from the stack.
      Makes the next popup in the stack the new active popup.
+
+     - Parameters:
+     - popupManagerID: The identifier for which the popup was presented. For more information, see ``Popup/present(popupManagerID:)``.
+
+     - Important: Make sure you use the correct **popupManagerID** from which you want to remove the popup.
      */
     func dismissLastPopup(popupManagerID: PopupManagerID = .shared) { PopupManager.dismissLastPopup(popupManagerID: popupManagerID) }
 
-    /// Dismisses all the popups of provided ID on the stack
+    /**
+     Removes all popups with the specified identifier from the stack.
+
+     - Parameters:
+     - id: Identifier of the popup located on the stack.
+     - popupManagerID: The identifier for which the popup was presented. For more information, see ``Popup/present(popupManagerID:)``.
+
+     - Important: Make sure you use the correct **popupManagerID** from which you want to remove the popup.
+     */
     func dismissPopup(_ id: String, popupManagerID: PopupManagerID = .shared) { PopupManager.dismissPopup(id, popupManagerID: popupManagerID) }
 
     /// Dismisses all the popups of provided type on the stack
