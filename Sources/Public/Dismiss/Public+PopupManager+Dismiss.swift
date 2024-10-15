bc180442710fb52bbcce27b3506fcc7c9b5b0b1d
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-16 01:29:03 +0200

    1

--- Sources/Public/Dismiss/Public+PopupManager+Dismiss.swift
+++ Sources/Public/Dismiss/Public+PopupManager+Dismiss.swift
@@ -31,6 +31,11 @@ public extension PopupManager {
 
     /**
      Removes all popups from the stack.
+
+     - Parameters:
+        - popupManagerID: The identifier for which the popup was presented. For more information, see ``Popup/present(popupManagerID:)``.
+
+     - Important: Make sure you use the correct **popupManagerID** from which you want to remove the popup.
      */
     static func dismissAllPopups(popupManagerID: PopupManagerID = .shared) { fetchInstance(id: popupManagerID)?.stack(.removeAllPopups) }
 }
