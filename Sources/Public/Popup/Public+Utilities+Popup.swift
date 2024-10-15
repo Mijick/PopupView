a87f86bbc989f9e1bfd23fbae0b64cf3c42cdc04
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-15 23:30:37 +0200

    1

--- Sources/Public/Popup/Public+Utilities+Popup.swift
+++ Sources/Public/Popup/Public+Utilities+Popup.swift
@@ -13,8 +13,26 @@ import Foundation
 
 // MARK: Height Mode
 public enum HeightMode {
+    /**
+     Popup height is calculated based on its content.
+
+     ## Visualisation
+     ![image](https://github.com/Mijick/Assets/blob/main/Framework%20Docs/Popups/height-mode-auto.png?raw=true)
+     */
     case auto
+
+    /**
+
+     ## Visualisation
+     ![image](https://github.com/Mijick/Assets/blob/main/Framework%20Docs/Popups/height-mode-large.png?raw=true)
+     */
     case large
+
+    /**
+     
+     ## Visualisation
+     ![image](https://github.com/Mijick/Assets/blob/main/Framework%20Docs/Popups/height-mode-fullscreen.png?raw=true)
+     */
     case fullscreen
 }
 
