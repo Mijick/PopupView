56c1c08a0c1616909cf2a63434267d113fc346d6
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-16 01:28:32 +0200

    1

--- Sources/Public/Dismiss/Public+PopupManager+Dismiss.swift
+++ Sources/Public/Dismiss/Public+PopupManager+Dismiss.swift
@@ -15,6 +15,11 @@ public extension PopupManager {
     /**
      Dismisses the currently active popup.
      Makes the next popup in the stack the new active popup.
+
+     - Parameters:
+        - popupManagerID: The identifier for which the popup was presented. For more information, see ``Popup/present(popupManagerID:)``.
+
+     - Important: Make sure you use the correct **popupManagerID** from which you want to remove the popup.
      */
     static func dismissLastPopup(popupManagerID: PopupManagerID = .shared) { fetchInstance(id: popupManagerID)?.stack(.removeLastPopup) }
 
@@ -24,6 +29,8 @@ public extension PopupManager {
     /// Dismisses all the popups of provided type on the stack
     static func dismissPopup<P: Popup>(_ type: P.Type, popupManagerID: PopupManagerID = .shared) { fetchInstance(id: popupManagerID)?.stack(.removeAllPopupsOfType(type)) }
 
-    /// Dismisses all the popups on the stack
+    /**
+     Removes all popups from the stack.
+     */
     static func dismissAllPopups(popupManagerID: PopupManagerID = .shared) { fetchInstance(id: popupManagerID)?.stack(.removeAllPopups) }
 }
