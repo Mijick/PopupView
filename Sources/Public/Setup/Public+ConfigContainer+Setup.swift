ef3b6256a7861e7102066e08783bded890155b22
Tomasz Kurylik <fulcrumone@icloud.com>
2024-10-15 11:19:54 +0200

    1

--- Sources/Public/Setup/Public+ConfigContainer+Setup.swift
+++ Sources/Public/Setup/Public+ConfigContainer+Setup.swift
@@ -10,6 +10,17 @@
 
 
 public extension GlobalConfigContainer {
+    /**
+     Default configuration for all centre popups.
+     Use the ``Popup/configurePopup(config:)-98ha0`` method to change the configuration for a specific popup.
+     See the list of available methods in ``GlobalConfig``.
+     */
     func centre(_ builder: (GlobalConfig.Centre) -> GlobalConfig.Centre) -> Self { Self.centre = builder(.init()); return self }
+
+    /**
+     Default configuration for all top and bottom popups.
+     Use the ``Popup/configurePopup(config:)-98ha0`` method to change the configuration for a specific popup.
+     See the list of available methods in ``GlobalConfig`` and ``GlobalConfig/Vertical``.
+     */
     func vertical(_ builder: (GlobalConfig.Vertical) -> GlobalConfig.Vertical) -> Self { Self.vertical = builder(.init()); return self }
 }
